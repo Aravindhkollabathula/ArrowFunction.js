6f86d96c76fe59ad2fc9339a1578b8bae3e925e2
Aravindhkollabathula <125636628+Aravindhkollabathula@users.noreply.github.com>
2023-04-06 15:13:14 +0530

    Add files via upload

---
+++ Arrow function.js
@@ -0,0 +1,38 @@
+//  function addition(){
+//     var a = Number(window.prompt("enter A value"))
+//     var b = Number(window.prompt("enter B value"))
+//     var sum = a+b
+//     // document.getElementById("result").innerHTML = "Result ="+sum
+//     console.log(`addition of ${a} and ${b} = ${sum}`)
+//     // console.log("addition of"+ a + "  and  "+ b + "=" + sum)
+//  }
+//  addition()
+
+// Arrow function for multiple line code
+
+// const add = () => {
+//     var a = Number(window.prompt("enter A value"))
+//     var b = Number(window.prompt("enter B value"))
+//     var sum = a+b
+    // document.getElementById("result").innerHTML = "Result ="+sum
+//     console.log(`addition of ${a} and ${b} = ${sum}`)
+// }
+// add()
+
+
+// function substraction(a,b){
+//     return a-b
+// }
+// var substractionresult = substraction(5,8)
+// console.log(substractionresult)
+
+
+// Arrow function in one line
+
+// const subtraction = (a,b) => console.log(a-b)
+// subtraction(5,8)
+
+//Arrow function:
+// const subt = (a,b) => a-b
+// var subtresult = subt(3,4)
+// console.log(subtresult)
